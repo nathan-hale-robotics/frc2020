1b6f84b82fb791c976da099bede9ab26c366bfce
Neil Macneale <neil.macneale.v@gmail.com>
2019-12-26 22:51:50 -0800

    Sends a simple message to server

--- jetson/main.py
+++ jetson/main.py
@@ -3,3 +3,12 @@ import grpc
 import vision_pb2
 import vision_pb2_grpc
 
+# open channel to localhost on port 8000
+channel = grpc.insecure_channel('localhost:8000')
+# tell grpc that this is a vision grpc channel
+stub = vision_pb2_grpc.VisionStub(channel)
+
+print("Sending SetMotorSpeed message...")
+res = stub.SetMotorSpeed(vision_pb2.MotorSpeed(left=3, right=2))
+print("Message sent successfully")
+

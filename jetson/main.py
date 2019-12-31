be96abedf5ec2b060c94f65e2aab288af4d4fc23
Neil Macneale <neil.macneale.v@gmail.com>
2019-12-30 18:22:10 -0800

    Added iterator to stream data to server

--- jetson/main.py
+++ jetson/main.py
@@ -3,12 +3,23 @@ import grpc
 import vision_pb2
 import vision_pb2_grpc
 
-# open channel to localhost on port 8000
+
+class MotorSpeedIterator:
+  def __init__(self):
+    self.right = 0
+    self.left = 0
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    self.right += 1
+    if self.right > 3:
+      raise StopIteration
+    return vision_pb2.MotorSpeed(left=self.left, right=self.right)
+
 channel = grpc.insecure_channel('localhost:8000')
-# tell grpc that this is a vision grpc channel
 stub = vision_pb2_grpc.VisionStub(channel)
 
-print("Sending SetMotorSpeed message...")
-res = stub.SetMotorSpeed(vision_pb2.MotorSpeed(left=3, right=2))
-print("Message sent successfully")
-
+for res in stub.SetMotorSpeed(MotorSpeedIterator()):
+  print("Got res:", res)

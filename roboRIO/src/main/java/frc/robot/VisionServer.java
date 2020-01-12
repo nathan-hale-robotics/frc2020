c6bedc7e941917a50ef0ec8d1f55af81e5174bdf
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-11 23:42:43 -0800

    Added spotless plugin

--- roboRIO/src/main/java/frc/robot/VisionServer.java
+++ roboRIO/src/main/java/frc/robot/VisionServer.java
@@ -3,11 +3,11 @@ package frc.robot;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class VisionServer extends Thread {
+
   private static final Logger logger = Logger.getLogger(VisionServer.class.getName());
   private final Robot robot;
 
@@ -17,9 +17,7 @@ public class VisionServer extends Thread {
 
   @Override
   public void run() {
-    Server server = ServerBuilder.forPort(8000)
-      .addService(new VisionImpl())
-      .build();
+    Server server = ServerBuilder.forPort(8000).addService(new VisionImpl()).build();
     try {
       server.start();
       server.awaitTermination();
@@ -31,12 +29,14 @@ public class VisionServer extends Thread {
   }
 
   private static class VisionImpl extends VisionGrpc.VisionImplBase {
+
     @Override
     public StreamObserver<MotorSpeed> setMotorSpeed(StreamObserver<MotorSpeedResponse> observer) {
-      return new StreamObserver<MotorSpeed>() {
+      return new StreamObserver<>() {
         @Override
         public void onNext(MotorSpeed speed) {
-          logger.info("Setting motor speed to: right: " + speed.getRight() + ", left: " + speed.getLeft());
+          logger.info(
+              "Setting motor speed to: right: " + speed.getRight() + ", left: " + speed.getLeft());
           MotorSpeedResponse.Builder res = MotorSpeedResponse.newBuilder();
           observer.onNext(res.build());
         }

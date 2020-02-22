0d5ca81c1e4dc1e6fbbf47481fe4d9063e003536
Neil Macneale <neil.macneale.v@gmail.com>
2020-02-22 15:52:48 -0800

    Added update camera iterator

--- jetson/main.py
+++ jetson/main.py
@@ -3,23 +3,46 @@ import grpc
 import vision_pb2
 import vision_pb2_grpc
 
+import cv2
 
-class MotorSpeedIterator:
+cap = cv2.VideoCapture(0)
+
+class MoveDirectionIterator:
   def __init__(self):
-    self.right = 0
-    self.left = 0
+    self.forward = 0
+    self.strafe = 0
+    self.turn = 0
 
   def __iter__(self):
     return self
 
   def __next__(self):
-    self.right += 1
-    if self.right > 3:
+    self.forward += 1
+    if self.forward > 3:
       raise StopIteration
-    return vision_pb2.MotorSpeed(left=self.left, right=self.right)
+    return vision_pb2.MoveDirection(forward=self.forward, strafe=self.strafe, turn=self.turn)
 
-channel = grpc.insecure_channel('localhost:8000')
+class CameraUpdateIterator:
+  def __init__(self):
+    pass
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    val, frame = cap.read()
+    array = bytearray(640 * 480 * 3)
+    width = 640
+    for y in range(480):
+      for x in range(640):
+        array[(y * width + x) * 3 + 0] = frame[y][x][0]
+        array[(y * width + x) * 3 + 1] = frame[y][x][1]
+        array[(y * width + x) * 3 + 2] = frame[y][x][2]
+    return vision_pb2.CameraUpdate(image=bytes(array))
+
+channel = grpc.insecure_channel('localhost:1234')
 stub = vision_pb2_grpc.VisionStub(channel)
 
-for res in stub.SetMotorSpeed(MotorSpeedIterator()):
-  print("Got res:", res)
+for res in stub.UpdateCamera(CameraUpdateIterator()):
+  print("REE:", res)
+

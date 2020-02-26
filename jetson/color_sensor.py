14151830baa97c25f4eed7512277d3f663f64d53
Neil Macneale <neil.macneale.v@gmail.com>
2020-02-25 18:38:38 -0800

    Changed to LAB colorspace, which is much more accurate

--- jetson/color_sensor.py
+++ jetson/color_sensor.py
@@ -6,27 +6,29 @@ def get_color(frame):
   print("Getting color")
 
   blur = cv2.blur(frame, (5, 5))
-  hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-  color = hsv.mean(axis=0).mean(axis=0)
-  hue = int(color[0])
+  lab = cv2.cvtColor(blur, cv2.COLOR_BGR2LAB)
+  frame_l,frame_a,frame_b = cv2.split(lab)
+  color = lab.mean(axis=0).mean(axis=0)
+  a = int(color[1])
+  b = int(color[2])
 
-  red = range(0, 20)
-  yellow = range(20, 40)
-  green = range(60, 85)
-  blue = range(85, 120)
+  red = (range(130, 200), range(140, 200))
+  yellow = (range(110, 130), range(130, 180))
+  green = (range(90, 110), range(120, 140))
+  blue = (range(110, 130), range(50, 130))
 
   color = vision_pb2.InfoUpdate.Color.UNDEFINED
   color_str = "undefined"
-  if hue in red:
+  if a in red[0] and b in red[1]:
     color = vision_pb2.InfoUpdate.Color.RED
     color_str = "red"
-  elif hue in yellow:
+  if a in yellow[0] and b in yellow[1]:
     color = vision_pb2.InfoUpdate.Color.YELLOW
     color_str = "yellow"
-  elif hue in green:
+  if a in green[0] and b in green[1]:
     color = vision_pb2.InfoUpdate.Color.GREEN
     color_str = "green"
-  elif hue in blue:
+  if a in blue[0] and b in blue[1]:
     color = vision_pb2.InfoUpdate.Color.BLUE
     color_str = "blue"
 
